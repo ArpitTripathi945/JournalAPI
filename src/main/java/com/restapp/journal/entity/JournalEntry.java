6e91612d52f9ee630965a8e9f6f3660c346bb5de
ArpitTripathi945 <ad3617@srmist.edu.in>
2024-07-16 02:39:48 +0530

    Database with backend connected

--- src/main/java/com/restapp/journal/entity/JournalEntry.java
+++ src/main/java/com/restapp/journal/entity/JournalEntry.java
@@ -1,32 +1,60 @@
 package com.restapp.journal.entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
 public class JournalEntry {
 
-    private long id;
+    @Id
+    private ObjectId id;
+
     private String title;
+
     private String content;
 
+    private LocalDateTime date;
+
+
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
-    public long getId() {
+    public ObjectId getId() {
+
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(ObjectId id) {
+
         this.id = id;
     }
 
     public String getContent() {
+
         return content;
     }
 
     public void setContent(String content) {
+
         this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 }
---
+++ src/main/java/com/restapp/journal/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package com.restapp.journal.repository;
+
+import com.restapp.journal.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId>{
+
+}
