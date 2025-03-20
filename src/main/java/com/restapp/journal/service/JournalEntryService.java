10b8852f7ea4eb4e68f63f3254cdf932d39af5bb
ArpitTripathi945 <ad3617@srmist.edu.in>
2025-03-20 12:36:30 +0530

    Spring Secuirty config

--- src/main/java/com/restapp/journal/service/JournalEntryService.java
+++ src/main/java/com/restapp/journal/service/JournalEntryService.java
@@ -1,11 +1,13 @@
 package com.restapp.journal.service;
 
 import com.restapp.journal.entity.JournalEntry;
+import com.restapp.journal.entity.User;
 import com.restapp.journal.repository.JournalEntryRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,29 +20,40 @@ public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
-    public void saveEntry(JournalEntry journalEntry){
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    public void saveEntry(JournalEntry journalEntry, String userName){
         try{
+            User user = userService.findByUserName(userName);
             journalEntry.setDate(LocalDateTime.now());
-            journalEntryRepository.save(journalEntry);
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
         }
         catch(Exception e){
-            log.error("Exception", e);
+            System.out.println(e);
+            throw new RuntimeException("An error occured while saving journal entry.", e);
         }
+    }
 
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
     }
 
     public List<JournalEntry> getAll(){
-
         return journalEntryRepository.findAll();
     }
 
     public Optional<JournalEntry> findById(ObjectId id){
-
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id){
-
+    public void deleteById(ObjectId id, String userName){
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
         journalEntryRepository.deleteById(id);
     }
 }
