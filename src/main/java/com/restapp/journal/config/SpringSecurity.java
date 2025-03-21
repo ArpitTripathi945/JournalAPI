308d2e2b73468aff7d4c28c55a9736d4d85c4419
ArpitTripathi945 <ad3617@srmist.edu.in>
2025-03-21 14:52:09 +0530

    Spring Secuirty Impl

--- src/main/java/com/restapp/journal/config/SpringSecurity.java
+++ src/main/java/com/restapp/journal/config/SpringSecurity.java
@@ -1,20 +1,34 @@
 package com.restapp.journal.config;
 
+import com.restapp.journal.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
 
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
+        http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/journal/**").authenticated()
                         .anyRequest().permitAll()
                 )
@@ -22,4 +36,19 @@ public class SpringSecurity {
 
         return http.build();
     }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(List.of(provider));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
